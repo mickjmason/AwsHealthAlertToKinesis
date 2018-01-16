c72274579d0ee7d126efede1a19e13700ebe1302
Mick Mason <p0rkskratchin@googlemail.com>
2018-01-16 08:45:31 +0000

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# AwsHealthAlertToKinesis
+# AwsHealthKinesis
 Terraform template for infrastructure needed to send AWS Health Notifications into a Kinesis Stream
+
+Rather than poll every instance to determine it's health status (which doesn't scale well at all), this set-up uses Cloudwatch Alarms to notify when an instances' health status changes.  The notifications are ultimately fed into a Kinesis Stream, where they can be collected using a Kinesis Consumer.
+
+There's no direct support for sending Cloudwatch alarm notifications to Kinesis, so this Terraform template builds the rather circuitous infrastructure neededed to achieve the goal, the flow is:
+ 
+Cloudwatch Alarm -> SNS -> SQS -> (which triggers) Lambda -> (which logs the SQS message to) Cloudwatch Logs -> (which has a subscription filter for) Kinesis.
+
+Current Limitations
+-------------------
+The Cloudwatch alarms have to be created on a per instance basis, and this Terraform template currently only creates the Alarm for a single hardcoded instance.  Later revisions will pull the list of instances from known Terraform state, or from an input file.
---
+++ health.tf
@@ -0,0 +1,149 @@
+
+resource "aws_instance" "test-instance" {
+  ami           = "ami-fcc4db98"
+  instance_type = "t2.micro"
+  key_name      = "my-key"
+}
+
+resource "aws_cloudwatch_metric_alarm" "test_alarm" {
+  alarm_name                = "test_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+  alarm_actions = [
+    "${aws_sns_topic.cloudwatch-alarms.arn}"
+  ]
+  dimensions {
+    InstanceId = "${aws_instance.test-instance.id}"
+  }
+}
+
+resource "aws_sns_topic" "cloudwatch-alarms" {
+  name = "cloudwatch-alarms"
+}
+
+resource "aws_sqs_queue" "test-queue" {
+  name = "test-queue"
+}
+
+resource "aws_sns_topic_subscription" "cloudwatchsubscription" {
+  topic_arn = "${aws_sns_topic.cloudwatch-alarms.arn}"
+  protocol  = "sqs"
+  endpoint  = "${aws_sqs_queue.test-queue.arn}"
+}
+
+resource "aws_iam_policy" "lambda_exec_policy" {
+  name        = "lambda_exec_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "sqs:DeleteMessage",
+        "sqs:ReceiveMessage"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:sqs:*"
+    },
+    {
+      "Action": [
+        "sqs:lambda:InvokeFunction"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_lambda_function.sqs_lambda.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "cloudwatch-to-kinesis-health-notifications" {
+  name			= "cloudwatch-to-kinesis-health-notifications"
+  policy		= <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "kinesis:PutRecord"
+      ],
+      "Resource":"${aws_kinesis_stream.healthstatus-stream.arn}"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role" "cloudwatch_kinesis_role" {
+  name = "cloudwatch_kinesis_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-exec-attachment" {
+  role = "${aws_iam_role.cloudwatch_kinesis_role.name}"
+  policy_arn = "${aws_iam_policy.lambda_exec_policy.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "kinesis-put-attachment" {
+  role = "${aws_iam_role.cloudwatch_kinesis_role.name}"
+  policy_arn = "${aws_iam_policy.cloudwatch-to-kinesis-health-notifications.arn}"
+}
+
+resource "aws_lambda_function" "sqs_lambda" {
+  filename         = "sqs_lambda.zip"
+  function_name    = "sqs_lambda"
+  role             = "${aws_iam_role.cloudwatch_kinesis_role.arn}"
+  handler          = "exports.handler"
+  source_code_hash = "${base64sha256(file("sqs_lambda.zip"))}"
+  runtime          = "nodejs4.3"
+
+  environment {
+    variables = {
+      queueUrl = "${aws_sqs_queue.test-queue.id}"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "healthnotifications-loggroup" {
+  name			= "healthnotifications-loggroup"
+}
+
+resource "aws_kinesis_stream" "healthstatus-stream" {
+  name			= "healthstatus-stream"
+  shard_count		= 1
+  retention_period	= 48
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "health-kinesis-filter" {
+  name			= "health-kinesis-filter"
+  role_arn		= "${aws_iam_role.cloudwatch_kinesis_role.name}"
+  log_group_name	= "${aws_cloudwatch_log_group.healthnotifications-loggroup.name}"
+  filter_pattern	= ""
+  destination_arn	= "${aws_kinesis_stream.healthstatus-stream.arn}"
+}
+
+
